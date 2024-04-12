31b530dea324790151646d4b96b7214bb5123c43
Omar Nassar <omariio1000@gmail.com>
2024-04-11 17:14:50 -0700

    start hw1

---
+++ hw1/01-hello.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', loadPage);
+
+function loadPage(event) {
+  console.log('The page has now loaded.');
+  console.log('My name is Omar and I am an undergrad in the last term of my senior year. I have a website that can be found at omarpdxpics.com.  My goal in taking this class is learning more about HTML, CSS, and JS so I can take it to the next level and become more fluent in web development!')
+}
