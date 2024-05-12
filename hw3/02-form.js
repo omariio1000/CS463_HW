b6fc3ceaf9bd0decfe5b941f565d113d2a777780
Omar Nassar <omariio1000@gmail.com>
2024-05-12 13:51:09 -0700

    small fixes

--- hw3/02-form.js
+++ hw3/02-form.js
@@ -5,22 +5,31 @@ let user = document.querySelector("#username");
 let email = document.querySelector("#email");
 let dob = document.querySelector("#dob");
 
-
 let pronouns;
 const radio = document.querySelectorAll('input[name="pronouns"]');
 radio.forEach((radio) => {
-    radio.addEventListener("click", (event) => {
-        pronouns = event.target.value;
-    })
-})
+  radio.addEventListener("click", (event) => {
+    pronouns = event.target.value;
+  });
+});
 
 submit.addEventListener("click", handleClick);
 
 function handleClick(event) {
-    event.preventDefault();
+  event.preventDefault();
+  if (
+    fullName.value === "" ||
+    username.value === "" ||
+    email.value === "" ||
+    dob.value === "" ||
+    pronouns === undefined
+  )
+    console.log("Must fill out the entire form!");
+  else {
     console.log("Name: ", fullName.value);
     console.log("Username: ", username.value);
     console.log("Email ", email.value);
     console.log("Date of Birth: ", dob.value);
     console.log("Pronouns: ", pronouns);
+  }
 }
